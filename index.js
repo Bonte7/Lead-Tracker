3cca41cc1b38d1e7f6e9b9aa0894050cf32cdaf9
Christian Bonte <cjbonte7@gmail.com>
2022-06-02 13:36:02 -0400

    implemented tab saves and lead deletion

--- index.js
+++ index.js
@@ -2,34 +2,25 @@ let myLeads = [];
 const saveInput = document.getElementById('input-btn');
 const inputEl = document.getElementById('input-el');
 const ulEl = document.getElementById('ul-el');
+const deleteInput = document.getElementById('delete-btn');
+const saveTabInput = document.getElementById('tab-btn');
 
-saveInput.addEventListener('click', handleSaveInput);
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-function handleSaveInput() {
-
-    if(myLeads.find(element => element === inputEl.value)) {
-        alert('This lead already exists!')
-    } else if(inputEl.value.length === 0) {
-        alert('Text field cannot be empty!')
-    } else {
-        myLeads.push(inputEl.value);
-        renderLeads();
-        inputEl.value = '';
-    }
-    
+if(leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    render(myLeads);
 }
 
-
-function renderLeads() {
+function render(renderItems) {
 
     let listItems = '';
 
-    for(let i = 0; i < myLeads.length; i++) {
-       
+    for(let i = 0; i < renderItems.length; i++) {
         listItems += 
         `<li>
-            <a href='https://${myLeads[i]}' target='_blank' rel='noopener noreferrer'>
-                ${myLeads[i]}
+            <a href='https://${renderItems[i]}' target='_blank' rel='noopener noreferrer'>
+                ${renderItems[i]}
             </a>
         </li>`;
     }
@@ -37,6 +28,58 @@ function renderLeads() {
     ulEl.innerHTML = listItems;
 
 }
+
+//manually save lead
+saveInput.addEventListener('click', function() {
+
+    if(checkForDuplicate(myLeads, inputEl.value) === false) {
+        myLeads.push(inputEl.value);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        render(myLeads);
+        inputEl.value = '';
+    }
+    
+});
+
+//save browser tab as lead
+saveTabInput.addEventListener('click', function() {
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+
+        if(checkForDuplicate(myLeads, tabs[0].url) === false) {
+            myLeads.push(tabs[0].url);
+            localStorage.setItem("myLeads", JSON.stringify(myLeads));
+            render(myLeads);
+        }
+   
+     });
+
+    
+    
+});
+
+//delete saved leads
+deleteInput.addEventListener('dblclick', function() {
+    myLeads = [];
+    localStorage.removeItem("myLeads");
+    render(myLeads)
+});
+
+//function to check for duplicate or empty lead values
+function checkForDuplicate(arrToCheck, valueToFind) {
+
+    if(arrToCheck.find(element => element === valueToFind)) {
+        alert('This lead already exists!')
+    } else if(valueToFind.length === 0) {
+        alert('Text field cannot be empty!')
+    } else {
+        return false;
+    }
+}
+
+
+
+
 
 
 
